5238e7348abb55a2a33f347b2839099bac24b0b8
Andrea Del Corto <andrea.delcorto@gmail.com>
2023-12-10 23:48:51 +0100

    day_3: add solution 1

--- advent_of_code_2023/day_3/__main__.py
+++ advent_of_code_2023/day_3/__main__.py
@@ -1,38 +1,91 @@
-from io import StringIO
+from dataclasses import dataclass
 from typing import IO
 import re
 
-def get_symbols_positions(row: str) -> list[int]:
-    return [m.start() for m in re.finditer(r"[^\d\.]", row)]
+
+@dataclass(frozen=True)
+class MatchedPart:
+    value: str
+    start: int
+    end: int
+
+
+@dataclass(frozen=True)
+class PartNumber:
+    value: int
+    row: int
+    start: int
+    end: int
+
+
+@dataclass(frozen=True)
+class Row:
+    index: int
+    value: str
+
+
+def get_start_end_positions_of_matches(row: str, pattern: str) -> list[MatchedPart]:
+    return [MatchedPart(m.group(), m.start(), m.end() - 1) for m in re.finditer(pattern, row)]
+
+
+def get_symbols(row: str) -> list[MatchedPart]:
+    return get_start_end_positions_of_matches(row, r"[^\d\.]")
+
+
+def get_numbers(row: str) -> list[MatchedPart]:
+    return get_start_end_positions_of_matches(row, r"\d+")
+
+
+def get_dots(row: str) -> list[MatchedPart]:
+    return get_start_end_positions_of_matches(row, r"\.")
+
+
+def is_symbol(value: str) -> bool:
+    return re.match(r"[^\d\.]", value) is not None
+
+
+def is_part_number(number_match: MatchedPart, candidate_positions: list[int]) -> bool:
+    return any(number_match.start <= pos <= number_match.end for pos in candidate_positions)
+
+
+def get_part_numbers(row: Row, candidate_positions: [int, int]) -> list[PartNumber]:
+    return [
+        PartNumber(int(number_match.value), row.index, number_match.start, number_match.end)
+        for number_match in get_numbers(row.value)
+        if is_part_number(number_match, candidate_positions)
+    ]
+
+
+def get_nearby_part_numbers(
+        symbol_index: int,
+        current_row: Row, previous_row: Row | None,
+        next_row: Row | None) -> set[PartNumber]:
+    nearby_part_numbers: set[PartNumber] = set()
+    for row in [previous_row, current_row, next_row]:
+        if row is not None:
+            candidate_part_numbers_positions = [
+                x for x in [symbol_index - 1, symbol_index, symbol_index + 1]
+                if x >= 0 and x < len(row.value)
+            ]
+            row_part_numbers = get_part_numbers(row, candidate_part_numbers_positions)
+            nearby_part_numbers.update(row_part_numbers)
+    return nearby_part_numbers
+
 
 def solve_1(input: IO) -> int:
-    rows = [row.strip() for row in input.readlines() if row.strip() != ""]
-    prev_row: str = ""
-    next_row: str = ""
-    num_rows: int = len(rows)
-
-    for idx, current_row in enumerate(rows):
-        prev_row = rows[idx - 1] if idx - 1 >= 0 else ""
-        next_row = rows[idx + 1] if idx + 1 < num_rows else ""
-        symbols_positions = get_symbols_positions(current_row)
-        for symbol_position in symbols_positions:
-            
-
-
-
+    raw_rows = [row.strip() for row in input.readlines() if row.strip() != ""]
+    rows = [Row(row_index, row.strip()) for row_index, row in enumerate(raw_rows)]
+    part_numbers: set[PartNumber] = set()
+    for current_row in rows:
+        next_row = rows[current_row.index + 1] if current_row.index + 1 < len(rows) else None
+        previous_row = rows[current_row.index - 1] if current_row.index - 1 >= 0 else None
+        for symbol in get_symbols(current_row.value):
+            nearby_part_numbers = get_nearby_part_numbers(symbol.start, current_row, previous_row, next_row)
+            part_numbers.update(nearby_part_numbers)
+    part_numbers_sum = sum([part_number.value for part_number in part_numbers])
+    return part_numbers_sum
 
 
 if __name__ == "__main__":
-    input: str = """
-    467..114..
-    ...*......
-    ..35..633.
-    ......#...
-    617*......
-    .....+.58.
-    ..592.....
-    ......755.
-    ...$.*....
-    .664.598..
-    """
-    solve_1(StringIO(input))
+    with open("input.txt") as f:
+        print(solve_1(f))
--- advent_of_code_2023/day_3/tests.py
+++ advent_of_code_2023/day_3/tests.py
@@ -1,6 +1,39 @@
 from io import StringIO
 
-from advent_of_code_2023.day_3.__main__ import solve_1
+from advent_of_code_2023.day_3.__main__ import solve_1, get_symbols, get_numbers, get_dots, \
+    MatchedPart
+
+
+def test_get_symbols():
+    input: str = "467..114..%&...23._"
+    assert get_symbols(input) == [
+        MatchedPart("%", 10, 10),
+        MatchedPart("&", 11, 11),
+        MatchedPart("_", 18, 18),
+    ]
+
+
+def test_get_numbers():
+    input: str = "467..114..%&...23._"
+    assert get_numbers(input) == [
+        MatchedPart("467", 0, 2),
+        MatchedPart("114", 5, 7),
+        MatchedPart("23", 15, 16),
+    ]
+
+
+def test_get_dots():
+    input: str = "467..114..%&...23._"
+    assert get_dots(input) == [
+        MatchedPart(".", 3, 3),
+        MatchedPart(".", 4, 4),
+        MatchedPart(".", 8, 8),
+        MatchedPart(".", 9, 9),
+        MatchedPart(".", 12, 12),
+        MatchedPart(".", 13, 13),
+        MatchedPart(".", 14, 14),
+        MatchedPart(".", 17, 17),
+    ]
 
 
 def test_solve_1():
@@ -17,5 +50,3 @@ def test_solve_1():
     .664.598..
     """
     assert solve_1(StringIO(input)) == 4361
-
-
