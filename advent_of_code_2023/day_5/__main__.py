1bc5bdbef6dab1d22b61a6e4951fa3d76e0f192f
Andrea Del Corto <andrea.delcorto@gmail.com>
2024-01-02 01:29:12 +0100

    day_5: add solution for part 2

--- advent_of_code_2023/day_5/__main__.py
+++ advent_of_code_2023/day_5/__main__.py
@@ -1,3 +1,4 @@
+import itertools
 import re
 from typing import IO
 from dataclasses import dataclass
@@ -49,6 +50,129 @@ class Almanac:
         return min(self.map(seed) for seed in self.seeds)
 
 
+@dataclass
+class RangeMapper:
+    source_range: range
+    destination_range: range
+
+    def map(self, range_to_map: range) -> list[range]:
+        mappable_range = self.get_mappable_range(range_to_map)
+
+        if not mappable_range:
+            return [range_to_map]
+
+        res = []
+        start_delta = mappable_range.start - self.source_range.start
+        end_delta = mappable_range.stop - self.source_range.start
+        res.append(range(
+            self.destination_range.start + start_delta,
+            self.destination_range.start + end_delta
+        ))
+
+        not_mapped_ranges = range_difference(range_to_map, mappable_range)
+        for not_mapped_range in not_mapped_ranges:
+            res.append(not_mapped_range)
+
+        return sorted(res, key=lambda x: x.start)
+
+    def get_mappable_range(self, range_to_map: range) -> range | None:
+        return range_intersect([range_to_map, self.source_range])
+
+
+@dataclass
+class RangeMultiMapper:
+    mappers: list[RangeMapper]
+
+    def map(self, range_to_map: range) -> list[range]:
+        res = []
+        mappable_ranges = []
+        for mapper in self.mappers:
+            mappable_range = mapper.get_mappable_range(range_to_map)
+            if mappable_range:
+                res += mapper.map(mappable_range)
+                mappable_ranges.append(mappable_range)
+
+        mappable_ranges = range_union(mappable_ranges)
+        res += range_multiple_difference(range_to_map, mappable_ranges)
+
+        return sorted(res, key=lambda x: (x.start, x.stop))
+
+
+@dataclass
+class RangeAlmanac:
+    seeds: list[range]
+    mappers: list[RangeMultiMapper]
+
+    def map(self, starting_range_to_map: range) -> list[range]:
+        mapped_ranges = [starting_range_to_map]
+        for mapper in self.mappers:
+            next_mapped_ranges = []
+            while mapped_ranges:
+                current_range_to_map = mapped_ranges.pop()
+                next_mapped_ranges += mapper.map(current_range_to_map)
+            mapped_ranges = next_mapped_ranges
+        return mapped_ranges
+
+    def get_lowest_location(self) -> int | None:
+        location_ranges: list[range] = list(itertools.chain.from_iterable((self.map(seed) for seed in self.seeds)))
+        if not location_ranges:
+            return None
+        return min(location_ranges, key=lambda x: x.start).start
+
+
+def range_difference(range1: range, range2: range) -> list[range]:
+    res = []
+    if range1.start > range2.stop - 1 or range1.stop - 1 < range2.start:
+        res = [range1]
+    elif range1.start < range2.start and range1.stop > range2.stop:
+        res = [
+            range(range1.start, range2.start),
+            range(range2.stop, range1.stop)
+        ]
+    elif range1.start < range2.start:
+        res = [range(range1.start, range2.start)]
+    elif range1.stop > range2.stop:
+        res = [range(range2.stop, range1.stop)]
+    return sorted(res, key=lambda x: x.start)
+
+
+def range_multiple_difference(range1: range, ranges: list[range]) -> list[range]:
+    res = [range1]
+    for range_to_remove in ranges:
+        idx = 0
+        while idx < len(res):
+            current_range = res[idx]
+            diff = range_difference(current_range, range_to_remove)
+            if diff != [current_range]:
+                res.pop(idx)
+                res = diff + res
+            idx += 1
+    return sorted(res, key=lambda x: x.start)
+
+
+def range_intersect(ranges: list[range]) -> range | None:
+    if len(ranges) == 0:
+        return None
+    intersection = ranges[0]
+    for current_range in ranges[1:]:
+        if intersection.start > current_range.stop - 1 or intersection.stop - 1 < current_range.start:
+            return None
+        intersection = range(max(intersection.start, current_range.start), min(intersection.stop, current_range.stop))
+    return intersection
+
+
+def range_union(ranges: list[range]) -> list[range]:
+    res: list[range] = []
+    for r in sorted(ranges, key=lambda x: (x.start, x.stop)):
+        begin = r.start
+        end = r.stop
+        if res and res[-1].stop - 1 >= begin - 1:
+            res[-1] = range(res[-1].start, max(res[-1].stop, end))
+        else:
+            res.append(range(begin, end))
+    return sorted(res, key=lambda x: x.start)
+
+
 def iterate_raw_rows(input: IO) -> list[str]:
     for row in input.readlines():
         if row.strip() != "":
@@ -81,6 +205,17 @@ def get_multimapper_from_file(rows: list[str], section_name: str) -> MultiMapper
     return MultiMapper(mappers)
 
 
+def get_range_multimapper_from_file(rows: list[str], section_name: str) -> RangeMultiMapper:
+    section_rows = get_file_section(rows, section_name)
+    mappers: list[RangeMapper] = []
+    for row in section_rows:
+        destination_range_start, source_range_start, range_length = [int(x) for x in row.split(" ")]
+        destination_range = range(destination_range_start, destination_range_start + range_length)
+        source_range = range(source_range_start, source_range_start + range_length)
+        mappers.append(RangeMapper(source_range, destination_range))
+    return RangeMultiMapper(mappers)
+
+
 def parse_input(input: IO) -> Almanac:
     rows = list(iterate_raw_rows(input))
     seeds: list[int] = [int(x) for x in rows[0][rows[0].find(":") + 1:].strip().split(" ")]
@@ -98,11 +233,41 @@ def parse_input(input: IO) -> Almanac:
     )
 
 
+def parse_input_range_almanac(input: IO) -> RangeAlmanac:
+    rows = list(iterate_raw_rows(input))
+    seeds: list[int] = [int(x) for x in rows[0][rows[0].find(":") + 1:].strip().split(" ")]
+    range_seeds: list[range] = []
+    for idx in range(0, len(seeds), 2):
+        start = seeds[idx]
+        length = seeds[idx + 1]
+        range_seeds.append(range(start, start + length))
+
+    return RangeAlmanac(
+        range_seeds,
+        [
+            get_range_multimapper_from_file(rows, "seed-to-soil map"),
+            get_range_multimapper_from_file(rows, "soil-to-fertilizer map"),
+            get_range_multimapper_from_file(rows, "fertilizer-to-water map"),
+            get_range_multimapper_from_file(rows, "water-to-light map"),
+            get_range_multimapper_from_file(rows, "light-to-temperature map"),
+            get_range_multimapper_from_file(rows, "temperature-to-humidity map"),
+            get_range_multimapper_from_file(rows, "humidity-to-location map")
+        ]
+    )
+
+
 def solve_1(input: IO) -> int:
     almanac = parse_input(input)
+    return almanac.get_lowest_location()
+
+
+def solve_2(input: IO) -> int:
+    almanac = parse_input_range_almanac(input)
     return almanac.get_lowest_location()
 
 
 if __name__ == "__main__":
     with open("input.txt") as f:
         print(f"Solution 1: {solve_1(f)}")
+        f.seek(0)
+        print(f"Solution 2: {solve_2(f)}")
--- advent_of_code_2023/day_5/tests.py
+++ advent_of_code_2023/day_5/tests.py
@@ -1,9 +1,11 @@
 from io import StringIO
 
-from advent_of_code_2023.day_5.__main__ import solve_1, Mapper, parse_input, Almanac, MultiMapper
+from advent_of_code_2023.day_5.__main__ import solve_1, Mapper, parse_input, Almanac, MultiMapper, solve_2, RangeMapper, \
+    range_intersect, RangeMultiMapper, RangeAlmanac, parse_input_range_almanac, range_difference, range_union, \
+    range_multiple_difference
 
 
-def test_category_mapper_destination_range():
+def test_mapper_destination_range():
     mapper = Mapper(destination_range_start=50, source_range_start=98, range_length=2)
     assert mapper.destination_range() == range(50, 52)
 
@@ -11,7 +13,7 @@ def test_category_mapper_destination_range():
     assert mapper.destination_range() == range(52, 100)
 
 
-def test_category_mapper_source_range():
+def test_mapper_source_range():
     mapper = Mapper(destination_range_start=50, source_range_start=98, range_length=2)
     assert mapper.source_range() == range(98, 100)
 
@@ -19,7 +21,7 @@ def test_category_mapper_source_range():
     assert mapper.source_range() == range(50, 98)
 
 
-def test_category_mapper_map_in_range():
+def test_mapper_map_in_range():
     mapper = Mapper(destination_range_start=50, source_range_start=98, range_length=2)
     assert mapper.map(98) == 50
     assert mapper.map(99) == 51
@@ -29,7 +31,7 @@ def test_category_mapper_map_in_range():
     assert mapper.map(70) == 72
 
 
-def test_category_mapper_map_out_of_range():
+def test_mapper_map_out_of_range():
     mapper = Mapper(destination_range_start=50, source_range_start=98, range_length=2)
     assert mapper.map(97) is None
     assert mapper.map(100) is None
@@ -115,6 +117,230 @@ def test_parse_input():
     )
 
 
+def test_range_mapper_map():
+    mapper = RangeMapper(source_range=range(10, 21), destination_range=range(30, 41))
+
+    assert mapper.map(range(8, 10)) == [range(8, 10)]
+    assert mapper.map(range(21, 23)) == [range(21, 23)]
+    assert mapper.map(range(9, 11)) == [range(9, 10), range(30, 31)]
+    assert mapper.map(range(9, 11)) == [range(9, 10), range(30, 31)]
+    assert mapper.map(range(20, 22)) == [range(21, 22), range(40, 41)]
+    assert mapper.map(range(9, 15)) == [range(9, 10), range(30, 35)]
+    assert mapper.map(range(15, 22)) == [range(21, 22), range(35, 41)]
+    assert mapper.map(range(11, 20)) == [range(31, 40)]
+    assert mapper.map(range(10, 21)) == [range(30, 41)]
+    assert mapper.map(range(9, 22)) == [range(9, 10), range(21, 22), range(30, 41)]
+
+
+def test_range_multi_mapper():
+    multi_mapper = RangeMultiMapper([
+        RangeMapper(source_range=range(10, 21), destination_range=range(40, 51)),
+        RangeMapper(source_range=range(30, 41), destination_range=range(60, 71)),
+    ])
+
+    assert multi_mapper.map(range(8, 10)) == [range(8, 10)]
+    assert multi_mapper.map(range(9, 11)) == [range(9, 10), range(40, 41)]
+    assert multi_mapper.map(range(9, 12)) == [range(9, 10), range(40, 42)]
+    assert multi_mapper.map(range(10, 12)) == [range(40, 42)]
+    assert multi_mapper.map(range(11, 20)) == [range(41, 50)]
+    assert multi_mapper.map(range(19, 21)) == [range(49, 51)]
+    assert multi_mapper.map(range(19, 22)) == [range(21, 22), range(49, 51)]
+    assert multi_mapper.map(range(20, 22)) == [range(21, 22), range(50, 51)]
+    assert multi_mapper.map(range(21, 30)) == [range(21, 30)]
+    assert multi_mapper.map(range(19, 32)) == [range(21, 30), range(49, 51), range(60, 62)]
+    assert multi_mapper.map(range(9, 42)) == [range(9, 10), range(21, 30), range(40, 51), range(41, 42), range(60, 71)]
+
+
+def test_intersect_ranges():
+    assert range_intersect([range(10, 21), range(8, 10)]) is None
+    assert range_intersect([range(10, 21), range(21, 23)]) is None
+    assert range_intersect([range(10, 21), range(9, 11)]) == range(10, 11)
+    assert range_intersect([range(10, 21), range(20, 22)]) == range(20, 21)
+    assert range_intersect([range(10, 21), range(9, 16)]) == range(10, 16)
+    assert range_intersect([range(10, 21), range(15, 22)]) == range(15, 21)
+    assert range_intersect([range(10, 21), range(11, 20)]) == range(11, 20)
+    assert range_intersect([range(10, 21), range(10, 21)]) == range(10, 21)
+    assert range_intersect([range(10, 21), range(9, 22)]) == range(10, 21)
+
+
+def test_union_ranges():
+    assert range_union([range(10, 21), range(5, 10)]) == [range(5, 21)]
+    assert range_union([range(10, 21), range(8, 10)]) == [range(8, 21)]
+    assert range_union([range(10, 21), range(21, 23)]) == [range(10, 23)]
+    assert range_union([range(10, 21), range(9, 11)]) == [range(9, 21)]
+    assert range_union([range(10, 21), range(20, 22)]) == [range(10, 22)]
+    assert range_union([range(10, 21), range(9, 16)]) == [range(9, 21)]
+    assert range_union([range(10, 21), range(15, 22)]) == [range(10, 22)]
+    assert range_union([range(10, 21), range(11, 20)]) == [range(10, 21)]
+    assert range_union([range(10, 21), range(10, 21)]) == [range(10, 21)]
+    assert range_union([range(10, 21), range(9, 22)]) == [range(9, 22)]
+
+
+def test_range_almanac_parse_input_range_almanac():
+    input = """
+    seeds: 79 14 55 13
+
+    seed-to-soil map:
+    50 98 2
+    52 50 48
+
+    soil-to-fertilizer map:
+    0 15 37
+    37 52 2
+    39 0 15
+
+    fertilizer-to-water map:
+    49 53 8
+    0 11 42
+    42 0 7
+    57 7 4
+
+    water-to-light map:
+    88 18 7
+    18 25 70
+
+    light-to-temperature map:
+    45 77 23
+    81 45 19
+    68 64 13
+
+    temperature-to-humidity map:
+    0 69 1
+    1 0 69
+
+    humidity-to-location map:
+    60 56 37
+    56 93 4
+    """
+    assert parse_input_range_almanac(StringIO(input)) == RangeAlmanac(
+        seeds=[range(79, 93), range(55, 68)],
+        mappers=[
+            RangeMultiMapper([
+                RangeMapper(source_range=range(98, 100), destination_range=range(50, 52)),
+                RangeMapper(source_range=range(50, 98), destination_range=range(52, 100))
+            ]),
+            RangeMultiMapper([
+                RangeMapper(source_range=range(15, 52), destination_range=range(0, 37)),
+                RangeMapper(source_range=range(52, 54), destination_range=range(37, 39)),
+                RangeMapper(source_range=range(0, 15), destination_range=range(39, 54))
+            ]),
+            RangeMultiMapper([
+                RangeMapper(source_range=range(53, 61), destination_range=range(49, 57)),
+                RangeMapper(source_range=range(11, 53), destination_range=range(0, 42)),
+                RangeMapper(source_range=range(0, 7), destination_range=range(42, 49)),
+                RangeMapper(source_range=range(7, 11), destination_range=range(57, 61))
+            ]),
+            RangeMultiMapper([
+                RangeMapper(source_range=range(18, 25), destination_range=range(88, 95)),
+                RangeMapper(source_range=range(25, 95), destination_range=range(18, 88))
+            ]),
+            RangeMultiMapper([
+                RangeMapper(source_range=range(77, 100), destination_range=range(45, 68)),
+                RangeMapper(source_range=range(45, 64), destination_range=range(81, 100)),
+                RangeMapper(source_range=range(64, 77), destination_range=range(68, 81)),
+            ]),
+            RangeMultiMapper([
+                RangeMapper(source_range=range(69, 70), destination_range=range(0, 1)),
+                RangeMapper(source_range=range(0, 69), destination_range=range(1, 70))
+            ]),
+            RangeMultiMapper([
+                RangeMapper(source_range=range(56, 93), destination_range=range(60, 97)),
+                RangeMapper(source_range=range(93, 97), destination_range=range(56, 60))
+            ])
+        ]
+    )
+
+
+def test_range_almanac():
+    input = """
+    seeds: 79 14 55 13
+    
+    seed-to-soil map:
+    50 98 2
+    52 50 48
+    
+    soil-to-fertilizer map:
+    0 15 37
+    37 52 2
+    39 0 15
+    
+    fertilizer-to-water map:
+    49 53 8
+    0 11 42
+    42 0 7
+    57 7 4
+    
+    water-to-light map:
+    88 18 7
+    18 25 70
+    
+    light-to-temperature map:
+    45 77 23
+    81 45 19
+    68 64 13
+    
+    temperature-to-humidity map:
+    0 69 1
+    1 0 69
+    
+    humidity-to-location map:
+    60 56 37
+    56 93 4
+    """
+    range_almanac: RangeAlmanac = parse_input_range_almanac(StringIO(input))
+    almanac: Almanac = parse_input(StringIO(input))
+
+    # Always out of range
+    assert range_almanac.map(range(-10, -5)) == [range(-10, -5)]
+    assert almanac.map(-1) == -1
+
+    assert almanac.map(79) == range_almanac.map(range(79, 80))[0].start
+
+
+def test_range_difference():
+    assert range_difference(range(10, 21), range(8, 10)) == [range(10, 21)]
+    assert range_difference(range(10, 21), range(21, 23)) == [range(10, 21)]
+    assert range_difference(range(10, 21), range(9, 11)) == [range(11, 21)]
+    assert range_difference(range(10, 21), range(20, 22)) == [range(10, 20)]
+    assert range_difference(range(10, 21), range(9, 16)) == [range(16, 21)]
+    assert range_difference(range(10, 21), range(15, 22)) == [range(10, 15)]
+    assert range_difference(range(10, 21), range(11, 20)) == [range(10, 11), range(20, 21)]
+    assert range_difference(range(10, 21), range(10, 21)) == []
+    assert range_difference(range(10, 21), range(9, 22)) == []
+
+
+def test_range_multiple_difference():
+    range1 = range(1, 5)
+    range2 = range(6, 10)
+    result = range_difference(range1, range2)
+    assert result == [range1]
+
+    range1 = range(1, 8)
+    range2 = range(5, 10)
+    result = range_difference(range1, range2)
+    assert result == [range(1, 5)]
+
+    range1 = range(1, 10)
+    range2 = range(3, 7)
+    result = range_difference(range1, range2)
+    assert result == [range(1, 3), range(7, 10)]
+
+    range1 = range(1, 5)
+    ranges_to_remove = [range(6, 10), range(12, 15)]
+    result = range_multiple_difference(range1, ranges_to_remove)
+    assert result == [range1]
+
+    range1 = range(1, 10)
+    ranges_to_remove = [range(5, 15), range(18, 20)]
+    result = range_multiple_difference(range1, ranges_to_remove)
+    assert result == [range(1, 5)]
+
+    range1 = range(1, 10)
+    ranges_to_remove = [range(3, 7), range(2, 8)]
+    result = range_multiple_difference(range1, ranges_to_remove)
+    assert result == [range(1, 2), range(8, 10)]
+
+
 def test_example_part_1():
     input: str = """
     seeds: 79 14 55 13
@@ -152,3 +378,42 @@ def test_example_part_1():
     56 93 4
     """
     assert solve_1(StringIO(input)) == 35
+
+
+def test_example_part_2():
+    input: str = """
+    seeds: 79 14 55 13
+
+    seed-to-soil map:
+    50 98 2
+    52 50 48
+
+    soil-to-fertilizer map:
+    0 15 37
+    37 52 2
+    39 0 15
+
+    fertilizer-to-water map:
+    49 53 8
+    0 11 42
+    42 0 7
+    57 7 4
+
+    water-to-light map:
+    88 18 7
+    18 25 70
+
+    light-to-temperature map:
+    45 77 23
+    81 45 19
+    68 64 13
+
+    temperature-to-humidity map:
+    0 69 1
+    1 0 69
+
+    humidity-to-location map:
+    60 56 37
+    56 93 4
+    """
+    assert solve_2(StringIO(input)) == 46
